a8c735c7dd6bbfc00508efd9968081a80da0dc0f
gilianopiassa <gilianopiassa@gmail.com>
2018-08-02 20:33:28 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php 
 	
 	use \Projeto_Ecommerce\Page;
+	use \Projeto_Ecommerce\Model\Product;
 
 	$app->get('/', function() {
+
+		$products = Product::listAll();
 	    
 	    $page = new Page();
 
-	    $page->setTpl("index");
+	    $page->setTpl("index", [
+	    	'products'=>Product::checkList($products)
+	    ]);
 
 	});
 
