2ace03521b2125525155f2d07a9e159504b0b1d3
gilianopiassa <gilianopiassa@gmail.com>
2018-08-28 20:52:27 -0300

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -68,7 +68,8 @@
 
 		$page->setTpl("cart", [
 			'cart'=>$cart->getValues(),
-			'products'=>$cart->getProducts()
+			'products'=>$cart->getProducts(),
+			'error'=>Cart::getMsgError()
 		]);
 	});
 
@@ -117,6 +118,17 @@
 
 		header("Location: /cart");
 		exit;
+	});
+
+	$app->post("/cart/freight", function(){
+
+		$cart = Cart::getFromSession();
+
+		$cart->setFreight($_POST['zipcode']);
+
+		header("Location: /cart");
+		exit;
+
 	});
 
 
