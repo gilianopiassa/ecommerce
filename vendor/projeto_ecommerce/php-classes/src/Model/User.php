dba1af789a07ff7c092568a7d73c905215500899
gilianopiassa <gilianopiassa@gmail.com>
2018-09-05 21:40:54 -0300

    Site Cadastro3

--- vendor/projeto_ecommerce/php-classes/src/Model/User.php
+++ vendor/projeto_ecommerce/php-classes/src/Model/User.php
@@ -65,7 +65,10 @@
 
 			$sql = new Sql();
 
-			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			$results = $sql->select("SELECT * FROM tb_users a 
+				INNER JOIN tb_persons b 
+				ON a.idperson = b.idperson 
+				WHERE a.deslogin = :LOGIN", array(
 				":LOGIN"=>$login
 			));
 
@@ -320,6 +323,22 @@
 		{
 
 			$_SESSION[User::ERROR_REGISTER] = $msg;
+		}
+
+		public static function getErrorRegister()
+		{
+
+			$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+			User::clearErrorRegister();
+
+			return $msg;
+		}
+
+		public static function clearErrorRegister()
+		{
+
+			$_SESSION[User::ERROR_REGISTER] = NULL;
 		}
 
 		public static function checkLoginExist($login)
