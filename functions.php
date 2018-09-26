d2748051d80e7f9b0afd0f11e161cef471886b5a
gilianopiassa <gilianopiassa@gmail.com>
2018-09-26 20:25:19 -0300

    Admin - Pedidos

--- functions.php
+++ functions.php
@@ -13,6 +13,12 @@
 		return number_format($vlprice, 2, ",", ".");
 	}
 
+	function formatDate($date)
+	{
+
+		return date('d/m/Y', strtotime($date));
+	}
+
 	function checkLogin($inadmin = true)
 	{
 
