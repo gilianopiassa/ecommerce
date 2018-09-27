1e240e03622478df5f30f6b0f84793416863a08e
gilianopiassa <gilianopiassa@gmail.com>
2018-09-27 19:25:40 -0300

    Admin - User - Busca e Paginação

--- admin-users.php
+++ admin-users.php
@@ -8,12 +8,42 @@
 
 		User::verifyLogin();
 
-		$users = User::listAll();
+		$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+
+		$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+		if ($search != '')
+		{
+
+			$pagination = User::getPageSearch($search, $page);
+		}
+		else
+		{
+
+			$pagination = User::getPage($page);
+		}		
+
+		$pages = [];
+
+		for ($x = 0; $x < $pagination['pages']; $x++)
+		{
+			array_push($pages, [
+				'href'=>'/admin/users?'.http_build_query([
+					'page'=>$x+1,
+					'search'=>$search
+				]),
+				'text'=>$x+1
+			]);
+		}
+
+		/* $users = User::listAll(); */
 
 		$page = new PageAdmin();
 
 		$page->setTpl("users", array(
-			"users"=>$users
+			"users"=>$pagination['data'],
+			"search"=>$search,
+			"pages"=>$pages
 		));
 	});
 
